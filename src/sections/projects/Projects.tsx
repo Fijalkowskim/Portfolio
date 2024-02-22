64664d5483b4ddd0f70e4eee8d7012e5ad21b7da
Mateusz Fijałkowski <fijalkowskim01@gmail.com>
2024-02-23 00:06:04 +0100

    GH pages setup

--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -8,7 +8,7 @@ function Projects() {
     <SectionBase name="Projects">
       <div className="flex flex-col items-center justify-start pt-28 gap-32 pb-10">
         {ProjectsList.map((proj, idx) => (
-          <ProjectCard key={idx} data={proj} inverted={idx % 2 == 1} />
+          <ProjectCard key={idx} data={proj} inverted={idx % 2 === 1} />
         ))}
       </div>
     </SectionBase>
